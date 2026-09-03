851b63fef08f148b763435d1d3d74463ba5a5a13
wasikkamila <wasikkamila3@gmail.com>
2026-09-03 11:33:53 +0200

    part9

---
+++ Server/InMemoryRepositories/CommentInMemoryRepository.cs
@@ -0,0 +1,70 @@
+﻿using Entities;
+using RepositoryContracts;
+
+namespace InMemoryRepositories;
+
+public class CommentInMemoryRepository : ICommentRepository
+{
+    private List<Comment> comments;
+    
+    public CommentInMemoryRepository()
+    {
+        comments = new List<Comment>();
+        
+        comments.Add(new Comment {Id = 1, Body = "Comment 1", UserId = 2, PostId = 1});
+        comments.Add(new Comment {Id = 2, Body = "Comment 2", UserId = 1, PostId = 2});
+    }
+    
+    public Task<Comment> AddAsync(Comment comment)
+    {
+        comment.Id = comments.Any()
+            ? comments.Max(c => c.Id) + 1
+            : 1;
+        comments.Add(comment);
+        return Task.FromResult(comment);
+    }
+    
+    public Task UpdateAsync(Comment comment)
+    {
+        Comment? existingComment = comments.SingleOrDefault(c => c.Id == comment.Id);
+        if (existingComment is null)
+        {
+            throw new InvalidOperationException(
+                $"Comment with ID '{comment.Id}' not found");
+        }
+
+        comments.Remove(existingComment);
+        comments.Add(comment);
+
+        return Task.CompletedTask;
+    }
+    
+    public Task DeleteAsync(int id)
+    {
+        Comment? commentToRemove = comments.SingleOrDefault(c => c.Id == id);
+        if (commentToRemove is null)
+        {
+            throw new InvalidOperationException(
+                $"Comment with ID '{id}' not found");
+        }
+
+        comments.Remove(commentToRemove);
+        return Task.CompletedTask;
+    }
+    
+    public Task<Comment> GetSingleAsync(int id)
+    {
+        Comment? comment = comments.SingleOrDefault(c => c.Id == id);
+        if (comment is null)
+        {
+            throw new InvalidOperationException(
+                $"Comment with ID '{id}' not found");
+        }
+        return Task.FromResult(comment);
+    }
+
+    public IQueryable<Comment> GetMany()
+    {
+        return comments.AsQueryable();
+    }
+}
