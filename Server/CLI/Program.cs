97a56fc1323a02ee6b9bff2eb9e1d30e5ce491b9
wasikkamila <wasikkamila3@gmail.com>
2026-09-07 09:11:56 +0200

    step 2.5

--- Server/CLI/Program.cs
+++ Server/CLI/Program.cs
@@ -1,1 +1,11 @@
-﻿Console.WriteLine("Hello, World!");
+﻿using CLI.UI;
+using InMemoryRepositories;
+using RepositoryContracts;
+
+Console.WriteLine("Starting CLI app...");
+IUserRepository userRepository = new UserInMemoryRepository();
+ICommentRepository commentRepository = new CommentInMemoryRepository();
+IPostRepository postRepository = new PostInMemoryRepository();
+
+CliApp cliApp = new CliApp(userRepository, commentRepository, postRepository);
+await cliApp.StartAsync();
